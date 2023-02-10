33c21cdf371299bf8438cd1232e3237f22b21767
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-10 20:21:02 +0900

    feat: TypeMismatchException 추가

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -39,6 +40,12 @@ public class ApiExceptionController {
     public String responseStatusEx2(){
         throw new ResponseStatusException(HttpStatus.NOT_FOUND,"error.bad",new IllegalArgumentException());
     }
+
+    @GetMapping("/api/default-status-ex")
+    public String defaultException(@RequestParam Integer data){
+        return "ok";
+    }
+
 
     @Data
     @AllArgsConstructor
