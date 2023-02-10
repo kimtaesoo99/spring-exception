ed5ef94f2a3a85a61f4ffe0ab1e2bf16babc5fa5
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-02-10 19:43:47 +0900

    feat: 사용자 오류 추가

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,5 +1,6 @@
 package hello.exception.api;
 
+import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +19,9 @@ public class ApiExceptionController {
         }
         if (id.equals("bad")){
             throw new IllegalArgumentException("잘못된 입력 값");
+        }
+        if (id.equals("user-ex")){
+            throw new UserException("사용자 오류");
         }
 
         return new MemberDto(id,"hello" + id);
